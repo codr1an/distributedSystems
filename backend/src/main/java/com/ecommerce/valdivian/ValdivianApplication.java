c3d29f096cd124b9409fe6d74ce45527a4c0820c
Adrian <adi_popa2002@yahoo.com>
2024-10-18 09:30:19 +0200

    init commit

---
+++ backend/src/main/java/com/ecommerce/valdivian/ValdivianApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.valdivian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValdivianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValdivianApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ecommerce/valdivian/ValdivianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.valdivian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValdivianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
