1bb5be18b34bb1f38299f998ce8a80a276383cd6
Adrian <adi_popa2002@yahoo.com>
2024-10-29 16:51:51 +0100

    start homepage cards

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,9 +1,67 @@
 import Navbar from "./Navbar";
+import phoneImage from "../../assets/iPhone.png";
+import "./Home.css";
+
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1>Home</h1>
+      <div class="category-presentation">
+        <div class="card-wrapper">
+          <div class="card" style={{ width: 300 }}>
+            <img src={phoneImage} class="card-img" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="/" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class="card" style={{ width: 300 }}>
+            <img src={phoneImage} class="card-img" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="/" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class="card" style={{ width: 300 }}>
+            <img src={phoneImage} class="card-img" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="/" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class="card" style={{ width: 300 }}>
+            <img src={phoneImage} class="card-img" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="/" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
