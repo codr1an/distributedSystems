be6e737a9178ec29920a470a1817a28f979b9a04
Adrian <adi_popa2002@yahoo.com>
2024-11-11 08:57:25 +0100

    updated homepage display

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -12,181 +12,45 @@ const Home = () => {
       <Navbar />
       <div class="category-presentation">
         <div class="card-wrapper">
-          <div class="card-pair">
-            <h1>Phones</h1>
-            <div
-              class="row row-cols-1 row-cols-md-2 g-1"
-              style={{ width: "19rem" }}
-            >
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={phoneImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">iPhone X</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={phoneImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">Samsung S20</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={phoneImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">Xiaomi Beijing</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={phoneImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">Huawei Milbei</h5>
-                </div>
-              </div>
+          <div class="card">
+            <div class="product-image">
+              <img src={phoneImage} class="card-img-top" alt="..." />
             </div>
+
+            <h5 class="card-title">iPhone</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="/" class="btn btn-primary">
+              To product
+            </a>
           </div>
-          <div class="card-pair">
-            <h1>Tvs</h1>
-            <div
-              class="row row-cols-1 row-cols-md-2 g-1"
-              style={{ width: "19rem" }}
-            >
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img src={tvImage} class="card-img-top mx-auto" alt="..." />
-                  <h5 class="card-title">iPhone X</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img src={tvImage} class="card-img-top mx-auto" alt="..." />
-                  <h5 class="card-title">Samsung S20</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img src={tvImage} class="card-img-top mx-auto" alt="..." />
-                  <h5 class="card-title">Xiaomi Beijing</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img src={tvImage} class="card-img-top mx-auto" alt="..." />
-                  <h5 class="card-title">Huawei Milbei</h5>
-                </div>
-              </div>
+          <div class="card">
+            <div class="product-image">
+              <img src={laptopImage} class="card-img-top" alt="..." />
             </div>
+            <h5 class="card-title">iPhone</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="/" class="btn btn-primary">
+              To product
+            </a>
           </div>
-          <div class="card-pair">
-            <h1>Monitors</h1>
-            <div
-              class="row row-cols-1 row-cols-md-2 g-1"
-              style={{ width: "19rem" }}
-            >
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={monitorImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">iPhone X</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={monitorImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">Samsung S20</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={monitorImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">Xiaomi Beijing</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={monitorImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">Huawei Milbei</h5>
-                </div>
-              </div>
+          <div class="card">
+            <div class="product-image">
+              <img src={laptopImage} class="card-img-top" alt="..." />
             </div>
-          </div>
-          <div class="card-pair">
-            <h1>Laptops</h1>
-            <div
-              class="row row-cols-1 row-cols-md-2 g-1"
-              style={{ width: "19rem" }}
-            >
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={laptopImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">iPhone X</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={laptopImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">Samsung S20</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={laptopImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">Xiaomi Beijing</h5>
-                </div>
-              </div>
-              <div class="col">
-                <div class="card h-100 border-white">
-                  <img
-                    src={laptopImage}
-                    class="card-img-top mx-auto"
-                    alt="..."
-                  />
-                  <h5 class="card-title">Huawei Milbei</h5>
-                </div>
-              </div>
-            </div>
+            <h5 class="card-title">iPhone</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="/" class="btn btn-outline-dark">
+              To product
+            </a>
           </div>
         </div>
       </div>
