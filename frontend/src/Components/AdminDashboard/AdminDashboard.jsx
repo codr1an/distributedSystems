fa9c7e0812c6ec4fea3ae020941531b62df186dd
codr1an <adi_popa2002@yahoo.com>
2024-11-17 17:07:29 +0100

    add sidebar for admin dashboard

--- frontend/src/Components/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/Components/AdminDashboard/AdminDashboard.jsx
@@ -1,8 +1,17 @@
 import "./AdminDashboard.css";
 import AdminNavbar from "./AdminNavbar";
+import Sidebar from "./Sidebar";
 
 const AdminDashboard = () => {
-  return <AdminNavbar />;
+  return (
+    <div>
+      <AdminNavbar />
+      <div className="admin-dashboard-container">
+        <Sidebar />
+        <div className="health-checks-container"></div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
---
+++ frontend/src/Components/AdminDashboard/Sidebar.jsx
@@ -0,0 +1,24 @@
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="side-bar">
+      <h1>User Management</h1>
+      <button type="button" class="btn btn">
+        <i class="bi bi-people"> View Users</i>
+      </button>
+      <button type="button" class="btn btn">
+        <i class="bi bi-person-add"> New User</i>
+      </button>
+      <h1>Product Management</h1>
+      <button type="button" class="btn btn">
+        <i class="bi bi-box-seam"> View Products</i>
+      </button>
+      <button type="button" class="btn btn">
+        <i class="bi bi-plus-lg"> New Product</i>
+      </button>
+    </div>
+  );
+};
+
+export default Sidebar;
